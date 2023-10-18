632f1ca8b223e2a0571e391ea94501aed20c9f69
MeiraYavetz <mayalea6745@gmail.com>
2023-10-18 11:36:58 +0300

    change include

---
+++ src/CLI/Encrypt_Decrypt/GlobalInstance.cpp
@@ -0,0 +1,6 @@
+#include "GlobalInstance.h"
+
+
+
+// Create an instance of SoftHSM
+SoftHSM* hsm = SoftHSM::i(); // Get the instance
--- src/CLI/Encrypt_Decrypt/GlobalInstance.h
+++ src/CLI/Encrypt_Decrypt/GlobalInstance.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "../../../src/lib/SoftHSM.h"
+#include "SoftHSM.h"
 
 #include <fcntl.h>
 #include <cstdlib>
